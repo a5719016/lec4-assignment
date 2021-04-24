92c845fc1674ee118e9913531cbdf8e9f234d5b1
kaz-kobayashi <50687254+kaz-kobayashi@users.noreply.github.com>
2021-04-24 17:11:00 +0900

    assignmend completed

---
+++ Exercise-4-problem-1.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Problem 1 - Simple temperature calculator (*3 points*)
+# 
+# In the first problem your aim is to create a function that converts the input temperature from degrees Fahrenheit to degrees Celsius. The conversion formula from Fahrenheit to Celsius can be found below.
+# 
+#   T_{Celsius} = ( T_{Fahrenheit} - 32 ) / 1.8
+# 
+# If everything in your script is working properly the following test case should work:
+# 
+# Notice: Closely follow the instructions! 
+# 
+# Your score on this problem will be based on following criteria:
+# 
+# - Creating a function called `fahr_to_celsius`
+# - Defining the function to have one input parameter called `temp_fahrenheit`
+# - Creating a variable called `converted_temp` inside the function to which you should assign the conversion result (i.e., the input Fahrenheit temperature converted to Celsius)
+# - Returning the converted value from the function back to the user
+# - Answering some questions about functions at the end of this problem
+# - Adding comments in your code and a docstring that explains how to use your `fahr_to_celsius` function (i.e., you should write the purpose of the function, parameters, and returned values)
+
+# YOUR CODE HERE
+
+
+# Us* your newly created function to print answers to the following questions:
+
+
+# 1. What is 48° Fahrenheit in Celsius? 
+
+# 2. What about 71° Fahrenheit in Celsius?
+
+
+# ### Problem 1 tests
+# 
+# Run the code cells below to check your answer. 
+
+# Check that the function produces correct answers for:
+# 1. What is 48° Fahrenheit in Celsius? 
+assert round(fahr_to_celsius(48), 2) == 8.89
+
+
+# 2. What about 71° Fahrenheit in Celsius?
+assert round(fahr_to_celsius(71), 2) == 21.67
+
+
+# ### Check your code
+# 
+# - Make sure you used the given variable names
+# - Check that you have added necessary comments to your code
+# - Check that your function has a docstring that describes what it does
+# 
+# ### Questions
+# 
+# We would like you to think about and answer the following questions based on the materials and ideas that you learned during the lecture:
+# 
+#   1. Is the concept of function clear to you? If not, what do you not understand?
+#   2. What are some of the benefits of using functions?
+#   
+# Write your answers below:
+
+# YOUR ANSWER HERE
+
+# #### Done!
+# 
+# That's it! Now you are ready to continue with Problem 2.
